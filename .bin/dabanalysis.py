4d1736071a33c69bc78ad836610c94f883a33140
c-arthurs <callumarthurs@yahoo.co.uk>
2020-11-19 21:14:08 +0000

    .

--- .bin/dabanalysis.py
+++ .bin/dabanalysis.py
@@ -6,4 +6,4 @@ import sys
 path = sys.argv[1]
 value = sys.argv[2]
 print(f"analysing {path} with threshold {value}")
-# main(path, value, segmented_image=False)
+main(path, value, segmented_image=False)
