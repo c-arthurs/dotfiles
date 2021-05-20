5c1cad9991107b7de8c05aef2f6d11b901c8210d
Callum Arthurs <callumarthurs@yahoo.co.uk>
2021-05-20 10:01:55 +0100

    update libraries

--- .bin/wsi_tissue_export.py
+++ .bin/wsi_tissue_export.py
@@ -5,17 +5,21 @@
 #     wsi_tissue_export "/path/to/file/dir/with/ndpi" --imagesize 1000 --imageoverlap 20 --annotation_segment --prep_ml_dataset
 
 
-from cnncore import deep_prep # pip install --upgrade git+ssh://git@github.com/c-arthurs/tma_lib.git#egg=tma_lib
+from wsi_toolbox import wsi_tissue_export #pip install --upgrade git+ssh://git@github.com/c-arthurs/wsi_toolbox.git#egg=wsi_toolbox
 import sys 
 import argparse
 
 if __name__ == "__main__":
     # Instantiate the parser
-    parser = argparse.ArgumentParser(description='Optional app description')
+    parser = argparse.ArgumentParser(
+    description="""for exporting tiles automatically using the cnncore tma_lib library that can be installed as
+    below:
+    wsi_tissue_export "/path/to/file/dir/with/ndpi" --imagesize 1000 
+    --imageoverlap 20 --annotation_segment --prep_ml_dataset""")
 
     # Required positional argument
     parser.add_argument('path', type=str, 
-            help='The path to the file ending in a /')
+            help='The path to the folder of wsi ending in a /')
     parser.add_argument('--imagesize', type=int, default=1024, 
                                 help='The size of the desired image patch')
     parser.add_argument('--imageoverlap', type=int, default=0, help='the amount tiles should overlap')
@@ -32,6 +36,6 @@ if __name__ == "__main__":
     results = [args.path, args.imagesize, args.imageoverlap, args.annotation_segment, args.prep_ml_dataset]
     [print(choicenames[i] + ": " + str(results[i])) for i in range(len(choicenames))]
 
-    deep_prep.main(str(args.path), annotation_segment=bool(args.annotation_segment), 
+    wsi_tissue_export.main(str(args.path), annotation_segment=bool(args.annotation_segment), 
             prep_ml_dataset=bool(args.prep_ml_dataset), imagesize=int(args.imagesize), 
             imageoverlap=int(args.imageoverlap), cover_percentage=float(args.cover_percentage))
